d20cffa3cdbe6d385292f8cefdb50416f3cfca9e
Severn <Severn@atguigu.com>
2022-09-23 21:55:17 +0800

    commit second

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git!!!！");
     }
 }
